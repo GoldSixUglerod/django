ddc06195cdaf3f941535b7948ab734ca280a2141
Dan1lD <davidian654@gmail.com>
2021-08-28 22:32:22 +0300

    Add get to employee api

--- src/api/views/EmployeeView.py
+++ src/api/views/EmployeeView.py
@@ -7,4 +7,22 @@ from ..models import Employee, Leader, Department
 
 class EmployeeView(APIView):
     def get(self, request, department_pk):
-        pass
+        res = {}
+        employees = Employee.objects.filter(department_id=department_pk)
+        root = employees.get(main=True)
+        res['name'] = root.name
+        res['attributes'] = root
+        res['children'] = []
+        children_id = Leader.objects.filter(leader_id=root.id).values_list('employee_id')
+        if children_id.count() > 0:
+            DFS(res, children_id, employees)
+        return res
+
+
+def DFS(res, children_id, employees):
+    for i in range(children_id.count()):
+        tmp_employee = employees.get(children_id[i][0])
+        res['children'].append({'name': tmp_employee['name'], 'attributes': tmp_employee, 'children': []})
+        leaders = Leader.objects.filter(leader_id=tmp_employee.id).values_list('employee_id').count()
+        if leaders != 0:
+                DFS(res['children'][i], leaders, employees)
