499f22a7ff1c17dbfa244cdd964ec4c17d945d13
Dan1lD <davidian654@gmail.com>
2021-08-28 03:15:28 +0300

    add poetry

--- src/ai_api/urls.py
+++ src/ai_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("notes/", views.NoteView.as_view()),
-    path("notes/<int:pk>", views.NoteView.as_view()),
+    # path("notes/", views.NoteView.as_view()),
+    # path("notes/<int:pk>", views.NoteView.as_view()),
 ]
--- src/api/models/Action.py
+++ src/api/models/Action.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django_postgres_extensions.models.fields import ArrayFields
 
 
 class Action(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    vector_name = models.FloatField()
+    vector_name = ArrayFields(models.FloatField)
     description = models.TextField(max_length=1000, null=True, default="")
