d6c8f69631cfa243ac835abddbc9daefc7c7df15
Dan1lD <davidian654@gmail.com>
2021-08-28 22:49:04 +0300

    Add paths

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-import ai_api
 from . import views
 
 urlpatterns = [
     path("department/", views.DepartmentView.as_view()),
     path("department/<int:pk>", views.DepartmentView.as_view()),
-
-
+    path("employee/", views.EmployeeView.as_view()),
+    path("employee/<int:pk>", views.EmployeeView.as_view()),
+    path("task/", views.TaskView.as_view()),
+    path("task/<int:pk>", views.TaskView.as_view()),
 ]
