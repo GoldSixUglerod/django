8e76f2baba536d2ad4738fdf81020e121f40f82d
SyrexMinus <makarsevchenko@gmail.com>
2021-08-28 19:37:37 +0300

    post task

--- src/api/views/TaskView.py
+++ src/api/views/TaskView.py
@@ -3,6 +3,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ..serializers import TaskSerializer
+from datetime import date, timedelta
 
 from ai_api.utils import KeywordsExtractor, vectorize_words
 from ..models import Employee, Leader, Department, Task
@@ -10,42 +11,59 @@ from ..models import Employee, Leader, Department, Task
 
 class TaskView(APIView):
     def get(self, request, pk):
-        task = Task.objects.all()
-        serializer = TaskSerializer(task)
-        return Response({"task": serializer.data})
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks)
+        return Response({"tasks": serializer.data})
 
     def post(self, request):
-        data = request.data
+        description = request.data.get("description")
+        end_time_best_req = request.data.get("end_time_best", {})
+        end_time_actual = None
+        finished = False
+        score = None
 
-        description = data['description']
-        if description == "" or description is None:
-            return Response({'status': 'error', 'description': "You have to provide a description"})
+
+        if description is None:
+            return Response({"status": "error", "description": "No description passed"})
 
         list_targets = KeywordsExtractor().extract(description)
+        name = " ".join(list_targets)
+        end_time_best = date.today() + timedelta(days=3)
+        if "year" in end_time_best_req and "month" in end_time_best_req and "day" in end_time_best_req:
+            end_time_best = date(
+                year=end_time_best_req["year"], month=end_time_best_req["month"], day=end_time_best_req["day"])
 
-        data['list_targets'] = list_targets
-        data['name'] = " ".join(list_targets)
+        data = {
+            "name": name,
+            "description": description,
+            "list_targets": list_targets,
+            "end_time_best": end_time_best,
+            "end_time_actual": end_time_actual,
+            "finished": finished,
+            "score": score
+        }
 
         serializer = TaskSerializer(data=data)
         if serializer.is_valid(raise_exception=True):
+            # TODO Assign task on department/employee
+
             saved_note = serializer.save()
-            return Response({"status": "success", "description": f"Task '{saved_note}' created successfully"})
-        return Response({"status": 'success', 'description': "Validate error"})
+            return Response({"status": "success", "description": f"Task \"{saved_note}\" created successfully"})
 
     def patch(self, request, pk):
         saved_task = get_object_or_404(Task.objects.all(), pk=pk)
         data = request.data
 
-        if data['list_targets']:
-            return Response({'status': 'error', 'description': "You can't change and list_targets of task"})
+        if data["list_targets"]:
+            return Response({"status": "error", "description": "You can't change and list_targets of task"})
 
         serializer = TaskSerializer(instance=saved_task, data=data, partial=True)
         if serializer.is_valid(raise_exception=True):
             saved_task = serializer.save()
-            return Response({"status": "success", "desciption": f"Task '{saved_task}' updated successfully"})
+            return Response({"status": "success", "desciption": f"Task \"{saved_task}\" updated successfully"})
         return Response({"status": "error", "desription": "Validate error"})
 
     def delete(self, request, pk):
         task = get_object_or_404(Task.objects.all(), pk=pk)
         task.delete()
-        return Response({"success": f"Task with id '{pk}' has been deleted."})
+        return Response({"success": f"Task with id \"{pk}\" has been deleted."})
