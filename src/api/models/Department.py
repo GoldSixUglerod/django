88681370d2ad8f43902970ee7d875444258f9273
SyrexMinus <makarsevchenko@gmail.com>
2021-08-29 13:07:28 +0300

    fix department

--- src/api/models/Department.py
+++ src/api/models/Department.py
@@ -19,7 +19,7 @@ class Department(models.Model):
         key_words_for_tasks = {}
 
         for task in tasks:
-            key_words = extractor.extract(task)  # Извлечение ключевых слов для конкретной задачи из писка задач
+            key_words, confidences = extractor.extract(task)  # Извлечение ключевых слов для конкретной задачи из писка задач
             for key_word in key_words:
                 # Добавление ключевого слова в словарь для подсчёта их количества
                 if key_word in key_words_for_tasks:
